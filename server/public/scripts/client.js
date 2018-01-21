c6ea51a8323eb59228df52a9626e2b6b5faf179c
Ross <ross@tests-MacBook-Pro.local>
2018-01-21 15:25:17 -0600

    Added toggle for each image and paragraph to describe the picture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,38 @@ const galleryApp = angular.module('galleryApp', []);
 
 galleryApp.controller('GalleryController', function(){
     const self = this;
+
+    self.pictures = [
+    self.pictureOne = {
+        toggle: true
+    },
+    self.pictureTwo = {
+        toggle: true
+    },
+    self.pictureThree = {
+        toggle: true
+    },
+    self.pictureFour = {
+        toggle: true
+    },
+    self.pictureFive = {
+        toggle: true
+    },
+    self.pictureSix = {
+        toggle: true
+    }
+]
+
+    self.toggleClick = function(picture){
+        if(picture.toggle == true){
+            picture.toggle = false;
+        } else{
+            picture.toggle = true;
+        }
+    }
     
+    self.likeButton = function(picture){
+        console.log('Clicked button for:', picture);
+        
+    }
 });
